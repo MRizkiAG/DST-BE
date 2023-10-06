acf61a294020e574f0e2adf812fb6d4d0d446377
MRizkiAG <mrizkiag1027@gmail.com>
2023-10-06 10:22:51 +0800

    Create API Auth and Protect Notes endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\NoteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,5 +20,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('notes', NoteController::class);
 
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+
+    Route::resource('notes', NoteController::class);
+});
